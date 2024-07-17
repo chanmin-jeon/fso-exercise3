e795280ced61a0735885b527ba8f5ceb64cd1499
Chanmin Jeon <chanminjeon@Chanmins-MBP.lan>
2024-07-16 18:36:07 -0600

    dist copied from frontend

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const app = express() 
-
 const morgan = require('morgan')
-
 const cors = require('cors')
 
-app.use(express.json())
 
 let persons = [
     { 
@@ -30,8 +27,10 @@ let persons = [
     }
 ]
 
+app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook!</h1>')
